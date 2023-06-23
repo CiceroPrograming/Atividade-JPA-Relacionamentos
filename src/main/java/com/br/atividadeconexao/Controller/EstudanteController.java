6887ab9b9bc16dd43d5de6b1b9d6eb5518b76a1f
Cícero <cicero.ferro.78@gmail.com>
2023-06-22 21:56:51 -0300

    Inserção HTML: cadastroEstudante, cadastroAula, cadastroProfessor

--- src/main/java/com/br/atividadeconexao/Controller/EstudanteController.java
+++ src/main/java/com/br/atividadeconexao/Controller/EstudanteController.java
@@ -1,5 +1,48 @@
 package com.br.atividadeconexao.Controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.br.atividadeconexao.Model.Aula;
+import com.br.atividadeconexao.Model.Estudante;
+import com.br.atividadeconexao.Model.Professor;
+import com.br.atividadeconexao.Repository.AulaRepository;
+import com.br.atividadeconexao.Repository.EstudanteRepository;
+import com.br.atividadeconexao.Repository.ProfessorRepository;
+
+@Controller
 public class EstudanteController {
+
+    @Autowired
+    EstudanteRepository repository;
+
+    @Autowired
+    AulaRepository aulaRepository;
+
+    @Autowired
+    ProfessorRepository professorRepository;
+
+    @GetMapping("/cadastroEstudante")
+    public ModelAndView cadastroEstudante() {
+        ModelAndView mv = new ModelAndView("cadastroEstudante");
+        ArrayList<Professor> professores = new ArrayList<>();
+        ArrayList<Aula> aulas = new ArrayList<>();
+        professores = (ArrayList<Professor>) professorRepository.findAll();
+        aulas = (ArrayList<Aula>) aulaRepository.findAll();
+        mv.addObject("aulas", aulas);
+        mv.addObject("professores", professores);
+        return mv;
+    }
+
+    @PostMapping("/cadastroEstudante")
+    public String salvar(Estudante estudante) {
+        repository.save(estudante);
+        return "redirect:/list";
+    }
 
 }
--- src/main/java/com/br/atividadeconexao/Controller/ProfessorController.java
+++ src/main/java/com/br/atividadeconexao/Controller/ProfessorController.java
@@ -1,5 +1,27 @@
 package com.br.atividadeconexao.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.br.atividadeconexao.Model.Professor;
+import com.br.atividadeconexao.Repository.ProfessorRepository;
+
+@Controller
 public class ProfessorController {
 
+    @Autowired
+    ProfessorRepository repository;
+
+    @GetMapping("cadastroProfessor")
+    public String cadastroProfessor() {
+        return "/cadastroProfessor";
+    }
+
+    @PostMapping
+    public String salvar(Professor professor) {
+        repository.save(professor);
+        return "redirect:/cadastroProfessor";
+    }
 }
